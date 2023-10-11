705bfea926259fc1c09e95b82752686af6d02c08
unknown <hediayari886@gmail.com>
2023-10-11 20:05:41 +0100

    test

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TP22222"
+include(":app")
+ 
